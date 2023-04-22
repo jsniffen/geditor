4584f1295822a7bd4fb3636b7e8a39b4fc2f2841
julian <julian.sniffen@gmail.com>
2023-04-21 23:37:11 -0400

    add linux init

---
+++ term/term_linux.go
@@ -0,0 +1,31 @@
+package term
+
+import "golang.org/x/sys/unix"
+import "os"
+
+func Init() error {
+	f, err := os.Open("/dev/tty")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	fd := int(f.Fd())
+
+	tios, err := unix.IoctlGetTermios(fd, unix.TCGETS)
+	if err != nil {
+		return err
+	}
+
+	tios.Iflag &= ^(uint32(unix.IGNBRK) | uint32(unix.BRKINT) | uint32(unix.PARMRK) | uint32(unix.ISTRIP) | uint32(unix.INLCR) | uint32(unix.IGNCR) | uint32(unix.ICRNL) | uint32(unix.IXON))
+        tios.Oflag &= ^uint32(unix.OPOST)
+        tios.Lflag &= ^(uint32(unix.ECHO) | uint32(unix.ECHONL) | uint32(unix.ICANON) | uint32(unix.ISIG) | uint32(unix.IEXTEN))
+        tios.Cflag &= ^(uint32(unix.CSIZE) | uint32(unix.PARENB))
+        tios.Cflag |= uint32(unix.CS8)
+        tios.Cc[unix.VMIN] = 1
+        tios.Cc[unix.VTIME] = 0
+
+	unix.IoctlSetTermios(fd, unix.TCSETA, tios)
+
+	return nil
+}
