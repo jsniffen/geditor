7e1091edf046a9c1beddabe03467744924641f01
julian <julian.sniffen@gmail.com>
2023-05-20 01:28:49 -0400

    add some impls for piece table insert and deletes

--- edit/piece_table.go
+++ edit/piece_table.go
@@ -1,5 +1,7 @@
 package edit
 
+import "fmt"
+
 type Entry struct {
 	original bool
 	idx      int
@@ -31,10 +33,80 @@ func NewPieceTable(contents string) (*PieceTable, error) {
 	return &pt, nil
 }
 
-func (pt *PieceTable) Insert(i int) {
+func (pt *PieceTable) Insert(b byte, i int) {
+	currEntry, n := pt.findEntry(i)
+
+	if currEntry == nil {
+		return
+	}
+
+	pt.add = append(pt.add, b)
+
+	newEntry := &Entry{
+		original: false,
+		idx: len(pt.add)-1,
+		len: 1,
+	}
+
+	if n == 0 {
+		// inserting at the beginning of entry
+		newEntry.prev = currEntry.prev
+		newEntry.next = currEntry
+		currEntry.prev = newEntry
+
+		if pt.table == currEntry {
+			pt.table = newEntry
+		}
+	} else if n == currEntry.len - 1 {
+		// inserting at the end of entry
+
+		// if we're adding to the end of an add buffer
+		// entry and it just so happens that this entry
+		// ends at the end of the add buffer, we can just extend
+		// this entry's length by one and call it a day.
+		if !currEntry.original && currEntry.idx + currEntry.len == len(pt.add) - 1 {
+			currEntry.len += 1
+		} else {
+			newEntry.next = currEntry.next
+			newEntry.prev = currEntry
+			currEntry.next = newEntry
+		}
+	} else {
+		// split the entry into 2
+		endEntry := &Entry{
+			original: currEntry.original,
+			idx: currEntry.idx+n,
+			len: currEntry.len-n,
+			next: currEntry.next,
+			prev: newEntry,
+		}
+
+		currEntry.len -= n
+		currEntry.next = newEntry
+
+		newEntry.prev = currEntry
+		newEntry.next = endEntry
+	}
 }
 
-func (pt *PieceTable) Delete(idx int) {
+func (pt *PieceTable) Delete(i int) {
+	currEntry, n := pt.findEntry(i)
+
+	if currEntry == nil {
+		return
+	}
+
+	if n == 0 {
+		// deleting at beginning of entry
+		currEntry.idx += 1
+		currEntry.len -= 1
+	} else if n == currEntry.len - 1 {
+		// deleting at end of entry
+		currEntry.len -= 1
+	} else {
+		// deleting in the middle of entry
+		// so split in 2
+	}
 }
 
 func (pt *PieceTable) String() string {
@@ -44,19 +116,37 @@ func (pt *PieceTable) String() string {
 		if entry.original {
 			b = pt.original
 		}
-		s += string(b[entry.idx:entry.len])
+		s += string(b[entry.idx:entry.idx+entry.len])
 	}
 	return s
 }
 
-func (pt *PieceTable) findEntry(idx int) (*Entry, int) {
+func (pt *PieceTable) findEntry(i int) (*Entry, int) {
+	if i < 0 {
+		return nil, 0
+	}
+
 	len := 0
 	for entry := pt.table; entry != nil; entry = entry.next {
-		if idx <= len + entry.len {
-			return entry, idx - len
+		if i < len + entry.len {
+			return entry, i - len
 		}
 
 		len += entry.len
 	}
 	return nil, 0
+}
+
+func (pt *PieceTable) printDebug() {
+	for entry := pt.table; entry != nil; entry = entry.next {
+		fmt.Println(entry)
+	}
+}
+
+func (pt *PieceTable) Len() int {
+	len := 0
+	for entry := pt.table; entry != nil; entry = entry.next {
+		len += 1
+	}
+	return len
 }
--- edit/piece_table_test.go
+++ edit/piece_table_test.go
@@ -19,14 +19,77 @@ func TestFindEntry(t *testing.T) {
 		entry *Entry
 		entryIndex int
 	}{
+		{-1, nil, 0},
 		{0, pt.table, 0},
+		{3, pt.table, 3},
+		{4, nil, 0},
 	}
 
 	for _, test := range tests {
 		entry, entryIndex := pt.findEntry(test.index)
 
+		if test.entry == nil && entry == nil {
+			continue
+		}
+
 		if entry != test.entry || entryIndex != test.entryIndex {
 			t.Errorf("want: %v, %v, got: %v, %v", &test.entry, test.entryIndex, &entry, entryIndex)
+		}
+	}
+}
+
+func TestInsert(t *testing.T) {
+	pt, _ := NewPieceTable("test")
+
+	tests := []struct {
+		index int 
+		b byte
+		want string
+		len int
+	}{
+		{-1, 'x', "test", 1},
+		// {5, 'x', "testx", 2},
+		// {0, 'x', "xtest", 2},
+		// {3, 'x', "xtexst", 4},
+		// {6, 'x', "xtexstx", 2},
+	}
+
+	for i, test := range tests {
+		pt.Insert(test.b, test.index)
+		got := pt.String()
+		if test.want != got {
+			t.Errorf("test %d: want: %s, got %s", i, test.want, got)
+		}
+
+		if test.len != pt.Len() {
+			t.Errorf("test %d: want len: %d, got %d", i, test.len, pt.Len())
+		}
+	}
+}
+
+func TestDelete(t *testing.T) {
+	pt, _ := NewPieceTable("tests")
+
+	tests := []struct {
+		index int 
+		want string
+		len int
+	}{
+		{-1, "tests", 1},
+		{5, "tests", 1},
+		{0, "ests", 1},
+		{3, "est", 1},
+	}
+
+	for i, test := range tests {
+		pt.Delete(test.index)
+		got := pt.String()
+		if test.want != got {
+			t.Errorf("test %d: want: %s, got %s", i, test.want, got)
+		}
+
+		if test.len != pt.Len() {
+			t.Errorf("test %d: want len: %d, got %d", i, test.len, pt.Len())
 		}
 	}
 }
